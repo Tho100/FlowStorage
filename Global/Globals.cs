00d6fc0a37f5fe36eb3b7c2461f3282ea02166d6
Tho100 <nfrealyt@gmail.com>
2023-11-08 22:44:49 +0800

    refactor: Removed unsupported file type from filter & and added xml support

--- Global/Globals.cs
+++ Global/Globals.cs
@@ -16,28 +16,28 @@ namespace FlowSERVER1 {
         static public string custEmail = "";
         static public string accountType = "";
 
-        static public readonly Image VerticalMenuImage = FlowSERVER1.Properties.Resources.icons8_menu_vertical_30;
-        static public readonly Image DirectoryGarbageImage = FlowSERVER1.Properties.Resources.icons8_garbage_66__1_;
-        static public readonly Image DIRIcon = FlowSERVER1.Properties.Resources.DirIcon;
+        static public readonly Image VerticalMenuImage = Properties.Resources.icons8_menu_vertical_30;
+        static public readonly Image DirectoryGarbageImage = Properties.Resources.icons8_garbage_66__1_;
+        static public readonly Image DIRIcon = Properties.Resources.DirIcon;
 
-        static public readonly Image PDFImage = FlowSERVER1.Properties.Resources.icons8_pdf_60__1_;
-        static public readonly Image AudioImage = FlowSERVER1.Properties.Resources.icons8_audio_file_60;
+        static public readonly Image PDFImage = Properties.Resources.icons8_pdf_60__1_;
+        static public readonly Image AudioImage = Properties.Resources.icons8_audio_file_60;
 
-        static public readonly Image DOCImage = FlowSERVER1.Properties.Resources.icons8_microsoft_word_60;
-        static public readonly Image PTXImage = FlowSERVER1.Properties.Resources.icons8_microsoft_powerpoint_60;
-        static public readonly Image APKImage = FlowSERVER1.Properties.Resources.icons8_microsoft_powerpoint_60;
+        static public readonly Image DOCImage = Properties.Resources.icons8_microsoft_word_60;
+        static public readonly Image PTXImage = Properties.Resources.icons8_microsoft_powerpoint_60;
+        static public readonly Image APKImage = Properties.Resources.icons8_microsoft_powerpoint_60;
 
-        static public readonly Image EXCELImage = FlowSERVER1.Properties.Resources.excelIcon;
-        static public readonly Image MSIImage = FlowSERVER1.Properties.Resources.icons8_software_installer_32;
-        static public readonly Image EXEImage = FlowSERVER1.Properties.Resources.icons8_exe_48;
+        static public readonly Image EXCELImage = Properties.Resources.excelIcon;
+        static public readonly Image MSIImage = Properties.Resources.icons8_software_installer_32;
+        static public readonly Image EXEImage = Properties.Resources.icons8_exe_48;
 
-        static private readonly Image TextImage = FlowSERVER1.Properties.Resources.icons8_txt_48;
-        static private readonly Image CSVImage = FlowSERVER1.Properties.Resources.icons8_csv_48;
-        static private readonly Image CssImage = FlowSERVER1.Properties.Resources.icons8_css_filetype_48__1_;
-        static private readonly Image JsImage = FlowSERVER1.Properties.Resources.icons8_javascript_50;
-        static private readonly Image HTMLImage = FlowSERVER1.Properties.Resources.icons8_html_filetype_48__1_;
-        static private readonly Image SQLImage = FlowSERVER1.Properties.Resources.icons8_database_50__1_;
-        static private readonly Image PYImage = FlowSERVER1.Properties.Resources.icons8_python_file_48;
+        static private readonly Image TextImage = Properties.Resources.icons8_txt_48;
+        static private readonly Image CSVImage = Properties.Resources.icons8_csv_48;
+        static private readonly Image CssImage = Properties.Resources.icons8_css_filetype_48__1_;
+        static private readonly Image JsImage = Properties.Resources.icons8_javascript_50;
+        static private readonly Image HTMLImage = Properties.Resources.icons8_html_filetype_48__1_;
+        static private readonly Image SQLImage = Properties.Resources.icons8_database_50__1_;
+        static private readonly Image PYImage = Properties.Resources.icons8_python_file_48;
 
         static public readonly HashSet<string> imageTypes = new HashSet<string> 
         {".png",".jpg",".jpeg"};
@@ -59,8 +59,6 @@ namespace FlowSERVER1 {
 
         static public readonly HashSet<string> ptxTypes = new HashSet<string>
         {".ppt",".pptx"};
-        //    --------- FILE TYPE WITHOUT END DOT
-
 
 
         static public readonly Dictionary<string, int> uploadFileLimit = new Dictionary<string, int>
@@ -97,7 +95,7 @@ namespace FlowSERVER1 {
             { ".html", HTMLImage},
             { ".sql", SQLImage},
             { ".py", PYImage},
-
+            { ".xml", TextImage},
         };
 
         static public readonly Dictionary<string, Image> textTypeToImageFolder = new Dictionary<string, Image>
@@ -110,9 +108,10 @@ namespace FlowSERVER1 {
             { "html", HTMLImage},
             { "sql", SQLImage},
             { "py", PYImage},
+            { "xml", TextImage},
         };
 
-        static public readonly string filterFileType = "All Files|*.*|Images Files|*.jpg;*.jpeg;*.png;|Video Files|*.mp4;*.webm;.mov;.wmv|Text Files|*.txt;*.md|Excel Files|*.xlsx;*.xls|Powerpoint Files|*.pptx;*.ppt|Word Documents|*.docx|Exe Files|*.exe|Audio Files|*.mp3;*.wav|Programming/Scripting|*.py;*.cs;*.cpp;*.java;*.php;*.js;|Markup Languages|*.html;*.css;*.xml|Acrobat Files|*.pdf|Comma Separated Values|*.csv";
+        static public readonly string filterFileType = "All Files|*.*|Image|*.jpg;*.jpeg;*.png;|Video|*.mp4;*.avi;.*mov;.*wmv;.*mkv|Text|*.txt;*.md|Excel|*.xlsx;*.xls|Powerpoint|*.pptx;*.ppt|Executable|*.exe|Audio Files|*.mp3;*.wav|Programming/Scripting|*.py;*.sql;*.js;|Markup|*.html;*.css;*.xml|Document|*.pdf;*.csv;*.docx";
 
     }
 }
